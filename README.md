253bb4a64775c6a5e249030c281667fb84105aa5
choon <chachooon@gmail.com>
2021-03-15 01:34:01 +0900

    feat: :sparkles: apple login

---
+++ README.md
@@ -0,0 +1,25 @@
+# Walki-client
+
+```bash
+ # appleAuthRequestResponse
+ {
+    authorizationCode:
+      'cd73eb637238a45889d79e5eccc4448ec.0.srqqz.OYyVIQwH-J1LFw1Y_jIwVA',
+    authorizedScopes: [],
+    email: 'chajy815@naver.com',
+    fullName: {
+      familyName: 'Hong',
+      givenName: 'GilDong',
+      middleName: null,
+      namePrefix: null,
+      nameSuffix: null,
+      nickname: null,
+    },
+    identityToken:
+      'eyJraWQiOiJlWGF1bm1MIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLnBhbG10cmVlLndhbGtpIiwiZXhwIjoxNjE1ODI1NjA5LCJpYXQiOjE2MTU3MzkyMDksInN1YiI6IjAwMTAwOS5hNmEyMGJjZDNjZjI0MzM0OTY2ZGRhY2QwZTJhZTY0Zi4xMzQ5Iiwibm9uY2UiOiJmZWVmNWU0NWFlMGJmNDVkMWRmNTc1MzE1Y2FiMjEyMTRhMWI5NmIyZTMwNmNlYWNmNGU3ZGNmNzgyNTEwNmJjIiwiY19oYXNoIjoieDF3Z2dBcThNZHdQQ3J1N2ZWdVVaUSIsImVtYWlsIjoiY2hhank4MTVAbmF2ZXIuY29tIiwiZW1haWxfdmVyaWZpZWQiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNjE1NzM5MjA5LCJub25jZV9zdXBwb3J0ZWQiOnRydWUsInJlYWxfdXNlcl9zdGF0dXMiOjJ9.t5BVbw5koZ-67dMTvENhhMXEkR5tNL2oNjLFwaVdL_aT7yVDNN2bljUndX-_rVYLmDP8rqUhmicM5p2imkRmEx26P8fd4c1C_xBBM73wqTJvFExPRQdu4DYCGE3oM08-XCnr5Wq8Yw_K9oSPmEGt0mu7F-6xiHfye49nMSkm8Vtvbvdl28mjaa8IPlyEZggXdpraMZ-l8xuuEIH6r_bZwmu8srJ4TAl1Xs4nP4FNnOrNfHcwXpNqv1qBvNgzsyt4XoZ5ndS24y60Dz6AXpkWZAfzHHuf2f1s2prbbebHwrAGYSdSAQPWLknbryQLRH6UJyni_u3Kum7Xj0cqiMRPLQ',
+    nonce: 'd1mPgoQRLeCJ3CbRDauiDTGQI0nLiA5D',
+    realUserStatus: 2,
+    state: null,
+    user: '001009.a6a20bcd3cf24334966ddacd0e2ae64f.1349',
+  };
+```
--- src/containers/auth/login.tsx
+++ src/containers/auth/login.tsx
@@ -1,14 +1,136 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/native';
+import { StyleSheet, View, Text } from 'react-native';
 import { isLoggedInVar } from '../../common/apollo';
+import {
+  appleAuth,
+  AppleButton,
+} from '@invertase/react-native-apple-authentication';
+
+/**
+ * You'd technically persist this somewhere for later use.
+ */
+let user: any = null;
+
+/**
+ * Fetches the credential state for the current user, if any, and updates state on completion.
+ */
+async function fetchAndUpdateCredentialState(
+  updateCredentialStateForUser: any,
+) {
+  if (user === null) {
+    updateCredentialStateForUser('N/A');
+  } else {
+    const credentialState = await appleAuth.getCredentialStateForUser(user);
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+      updateCredentialStateForUser('AUTHORIZED');
+    } else {
+      updateCredentialStateForUser(credentialState);
+    }
+  }
+}
+
+/**
+ * Starts the Sign In flow.
+ */
+async function onAppleButtonPress(updateCredentialStateForUser: any) {
+  console.warn('Beginning Apple Authentication');
+
+  // start a login request
+  try {
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+    });
+
+    console.log('appleAuthRequestResponse', appleAuthRequestResponse);
+
+    const {
+      user: newUser,
+      email,
+      nonce,
+      identityToken,
+      realUserStatus /* etc */,
+    } = appleAuthRequestResponse;
+
+    user = newUser;
+
+    fetchAndUpdateCredentialState(updateCredentialStateForUser).catch((error) =>
+      updateCredentialStateForUser(`Error: ${error.code}`),
+    );
+
+    if (identityToken) {
+      // e.g. sign in with Firebase Auth using `nonce` & `identityToken`
+      console.log(nonce, identityToken);
+    } else {
+      // no token - failed sign-in?
+    }
+
+    if (realUserStatus === appleAuth.UserStatus.LIKELY_REAL) {
+      console.log("I'm a real person!");
+    }
+
+    console.warn(`Apple Authentication Completed, ${user}, ${email}`);
+  } catch (error) {
+    if (error.code === appleAuth.Error.CANCELED) {
+      console.warn('User canceled Apple Sign in.');
+    } else {
+      console.error(error);
+    }
+  }
+}
 
 const LoginContainer = () => {
+  const [credentialStateForUser, updateCredentialStateForUser] = useState<any>(
+    -1,
+  );
+  useEffect(() => {
+    if (!appleAuth.isSupported) return;
+
+    fetchAndUpdateCredentialState(updateCredentialStateForUser).catch((error) =>
+      updateCredentialStateForUser(`Error: ${error.code}`),
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!appleAuth.isSupported) return;
+
+    return appleAuth.onCredentialRevoked(async () => {
+      console.warn('Credential Revoked');
+      fetchAndUpdateCredentialState(
+        updateCredentialStateForUser,
+      ).catch((error) => updateCredentialStateForUser(`Error: ${error.code}`));
+    });
+  }, []);
+
+  if (!appleAuth.isSupported) {
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <Text>Apple Authentication is not supported on this device.</Text>
+      </View>
+    );
+  }
+
   return (
-    <Container>
-      <Button onPress={() => isLoggedInVar(true)}>
-        <Text>Login</Text>
-      </Button>
-    </Container>
+    <View style={[styles.container, styles.horizontal]}>
+      <Text style={styles.header}>Credential State</Text>
+      <Text>{credentialStateForUser}</Text>
+
+      <AppleButton
+        style={styles.appleButton}
+        cornerRadius={5}
+        buttonStyle={AppleButton.Style.BLACK}
+        buttonType={AppleButton.Type.CONTINUE}
+        onPress={() => onAppleButtonPress(updateCredentialStateForUser)}
+      />
+      <AppleButton
+        style={styles.appleButton}
+        cornerRadius={5}
+        buttonStyle={AppleButton.Style.BLACK}
+        buttonType={AppleButton.Type.SIGN_IN}
+        onPress={() => onAppleButtonPress(updateCredentialStateForUser)}
+      />
+    </View>
   );
 };
 
@@ -22,4 +144,27 @@ const Container = styled.View`
 
 const Button = styled.TouchableOpacity``;
 
-const Text = styled.Text``;
+const styles = StyleSheet.create({
+  appleButton: {
+    width: 200,
+    height: 60,
+    margin: 10,
+  },
+  header: {
+    margin: 10,
+    marginTop: 30,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'pink',
+  },
+  horizontal: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+  },
+});
