43644c73903ed95e3781ded24c81033fbbec807d
choon <chachooon@gmail.com>
2021-04-03 23:49:58 +0900

    refactor: LoginContainer 분리

--- src/containers/auth/Guide.tsx
+++ src/containers/auth/Guide.tsx
@@ -7,15 +7,16 @@ import {
   Alert,
   TouchableOpacity,
   Text,
+  TouchableWithoutFeedbackProps,
 } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import styled from '@emotion/native';
-import { SafeLayout } from '../../layouts/SafeLayout';
+import LoginContainer from './Login';
 
 interface IGuideData {
   title: string;
-  subtitle: string;
-  source: string;
+  subTitle: string;
+  source: any;
   style: any;
 }
 
@@ -24,7 +25,7 @@ const { width: screenWidth, height: screenHight } = Dimensions.get('window');
 const GUIDE_DATA: IGuideData[] = [
   {
     title: '코치와 함께 러닝해보세요!',
-    subtitle: '토키와 부키가 당신의 데일리 러닝을 \n도와줘요!',
+    subTitle: '토키와 부키가 당신의 데일리 러닝을 \n도와줘요!',
     source: require('../../assets/images/img_onboarding_01.png'),
     style: {
       width: screenWidth,
@@ -33,14 +34,14 @@ const GUIDE_DATA: IGuideData[] = [
   },
   {
     title: '챌린지를 생성하고 달성해보세요!',
-    subtitle:
+    subTitle:
       '매일 챌린지를 달성하다보면 \n어느새 러닝이 습관으로 자리잡아 있을거예요!',
     source: require('../../assets/images/img_onboarding_02.png'),
     style: { width: screenWidth, height: screenHight / 2.1 },
   },
   {
     title: '메달을 차지해보세요!',
-    subtitle:
+    subTitle:
       '오늘 메달을 차지하지 못해도 낙심하지마세요! \n매일 기회가 주어져요!',
     source: require('../../assets/images/img_onboarding_03.png'),
     style: { width: screenWidth, height: screenHight / 2.5 },
@@ -49,125 +50,99 @@ const GUIDE_DATA: IGuideData[] = [
 
 const GuideContainer = () => {
   const [selectedCoachIndex, setSelectedCoachIndex] = useState(0);
-
   const carouselRef = useRef<Carousel<IGuideData>>(null);
-  const _renderItem = useCallback(({ item }: { item: any }) => {
-    return (
-      <Content>
-        <Title>{item.title}</Title>
-        <Subtitle>{item.subtitle}</Subtitle>
-        <Image
-          source={item.source}
-          style={{
-            ...item.style,
-            resizeMode: 'contain',
-          }}
-        />
-      </Content>
-    );
-  }, []);
+
+  const _renderItem = useCallback(
+    ({ item, index }: { item: IGuideData; index: number }) => {
+      return (
+        <>
+          <Text
+            numberOfLines={1}
+            adjustsFontSizeToFit
+            style={{
+              textAlign: 'left',
+              fontWeight: '600',
+              fontSize: 24,
+              lineHeight: 36,
+              marginBottom: 10,
+              marginLeft: 16,
+            }}>
+            {item.title}
+          </Text>
+          <Text
+            numberOfLines={2}
+            adjustsFontSizeToFit
+            style={{
+              textAlign: 'left',
+              fontWeight: '400',
+              fontSize: 16,
+              lineHeight: 24,
+              marginBottom: 70,
+              marginLeft: 16,
+            }}>
+            {item.subTitle}
+          </Text>
+
+          <Image
+            source={item.source}
+            style={{
+              ...item.style,
+              resizeMode: 'contain',
+            }}
+          />
+        </>
+      );
+    },
+    [],
+  );
 
   return (
-    <SafeLayout>
-      <Container>
-        <View
-          style={{
-            position: 'absolute',
-            width: '100%',
-            paddingTop: 50,
-            zIndex: 100,
-          }}>
-          <Pagination
-            dotsLength={3}
-            activeDotIndex={selectedCoachIndex}
-            dotColor={'#F22764'}
-            inactiveDotColor={'#E0E0E0'}
-            inactiveDotScale={1}
-          />
-        </View>
+    <Container>
+      <Pagination
+        dotsLength={3}
+        dotColor={'#F22764'}
+        inactiveDotColor={'#E0E0E0'}
+        inactiveDotScale={1}
+        activeDotIndex={selectedCoachIndex}
+        containerStyle={{
+          marginTop: 80,
+        }}
+      />
 
-        <Carousel
-          ref={carouselRef}
-          data={GUIDE_DATA}
-          renderItem={_renderItem}
-          sliderWidth={screenWidth}
-          sliderHeight={screenHight}
-          itemWidth={screenWidth}
-          onSnapToItem={(index) => setSelectedCoachIndex(index)}
-        />
+      <Carousel
+        ref={carouselRef}
+        data={GUIDE_DATA}
+        renderItem={_renderItem}
+        sliderWidth={screenWidth}
+        sliderHeight={screenHight}
+        itemWidth={screenWidth}
+        onSnapToItem={(index) => setSelectedCoachIndex(index)}
+      />
 
-        <View style={styles.buttonContainer}>
-          {selectedCoachIndex < 2 ? (
-            <View style={styles.textButtonWrapper}>
-              {selectedCoachIndex === 0 && (
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  onPress={() => {
-                    if (carouselRef.current) carouselRef.current.snapToItem(2);
-                    setSelectedCoachIndex(2);
-                  }}>
-                  <Text style={styles.textButton}>건너뛰기</Text>
-                </TouchableOpacity>
-              )}
-              <View style={{ flexGrow: 1 }}></View>
-              <TouchableOpacity
-                activeOpacity={0.8}
-                onPress={() => {
-                  if (carouselRef.current)
-                    carouselRef.current.snapToItem(selectedCoachIndex + 1);
-                  setSelectedCoachIndex(selectedCoachIndex + 1);
-                }}>
-                <Text style={styles.textButton}>다음</Text>
-              </TouchableOpacity>
-            </View>
-          ) : (
-            <View style={styles.loginButtonWrapper}>
-              <TouchableOpacity
-                activeOpacity={0.8}
-                onPress={() => Alert.alert('카카오로 시작하기')}
-                style={{
-                  width: 300,
-                  height: 54,
-                  borderRadius: 8,
-                  backgroundColor: '#FFE812',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text>카카오로 시작하기</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                activeOpacity={0.8}
-                onPress={() => Alert.alert('애플로 시작하기')}
-                style={{
-                  width: 300,
-                  height: 54,
-                  borderRadius: 8,
-                  backgroundColor: '#000000',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text style={{ color: '#ffffff' }}>애플로 시작하기</Text>
-              </TouchableOpacity>
-              <Text style={{ textAlign: 'center', lineHeight: 15 }}>
-                walki의{' '}
-                <Text
-                  style={{ display: 'flex', textDecorationLine: 'underline' }}
-                  onPress={() => Alert.alert('이용약관')}>
-                  이용약관
-                </Text>
-                과{' '}
-                <Text
-                  style={{ display: 'flex', textDecorationLine: 'underline' }}
-                  onPress={() => Alert.alert('개인정보처리방침')}>
-                  개인정보처리방침
-                </Text>
-                을 읽고 이해했으며{'\n'}그에 동의함을 확인합니다.
-              </Text>
-            </View>
-          )}
-        </View>
-      </Container>
-    </SafeLayout>
+      <BottomContainer>
+        {selectedCoachIndex < 2 ? (
+          <TextButtonWrapper>
+            <TextButton
+              onPress={() => {
+                if (carouselRef.current) carouselRef.current.snapToItem(2);
+                setSelectedCoachIndex(2);
+              }}>
+              건너뛰기
+            </TextButton>
+            <TextButton
+              onPress={() => {
+                if (carouselRef.current)
+                  carouselRef.current.snapToItem(selectedCoachIndex + 1);
+                setSelectedCoachIndex(selectedCoachIndex + 1);
+              }}>
+              다음
+            </TextButton>
+          </TextButtonWrapper>
+        ) : (
+          <LoginContainer />
+        )}
+      </BottomContainer>
+    </Container>
   );
 };
 
@@ -177,44 +152,30 @@ const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.color.white};
 `;
-
-const Content = styled.View`
-  margin: 100px 0px;
+const BottomContainer = styled.View`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  z-index: 100;
+`;
+const TextButtonWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 70px 20px;
 `;
 
-const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  margin: 10px 16px;
-  line-height: 36px;
-`;
-
-const Subtitle = styled.Text`
-  font-size: 16px;
-  margin: 0 16px;
-  margin-bottom: 50px;
-`;
-const styles = StyleSheet.create({
-  textButtonWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 20,
-    paddingBottom: 70,
-  },
-  textButton: {
-    color: '#333333',
-    fontSize: 14,
-  },
-
-  loginButtonWrapper: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    position: 'absolute',
-    width: '100%',
-    bottom: 0,
-    zIndex: 100,
-  },
-});
+const TextButton: React.FC<TouchableWithoutFeedbackProps> = ({
+  onPress,
+  children,
+}) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+      <Text
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        style={{ color: '#333333', fontSize: 14 }}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -1,25 +1,113 @@
 import React from 'react';
 import styled from '@emotion/native';
 import { isLoggedInVar } from '../../common/apollo';
+import {
+  Alert,
+  Image,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedbackProps,
+} from 'react-native';
 
 const LoginContainer = () => {
   return (
-    <Container>
-      <Button onPress={() => isLoggedInVar(true)}>
-        <Text>Login</Text>
-      </Button>
-    </Container>
+    <LoginButtonWrapper>
+      <LoginButton
+        type="kakao"
+        onPress={() => Alert.alert('카카오로 시작하기')}
+      />
+      <LoginButton
+        type="apple"
+        onPress={() => Alert.alert('애플로 시작하기')}
+      />
+
+      <Text
+        numberOfLines={2}
+        adjustsFontSizeToFit
+        style={{
+          marginBottom: 42,
+          letterSpacing: 0.16,
+          lineHeight: 15,
+          fontSize: 12,
+          color: '#828282',
+          textAlign: 'center',
+        }}>
+        walki의{' '}
+        <TextLink onPress={() => Alert.alert('이용약관')}>이용약관</TextLink>과{' '}
+        <TextLink onPress={() => Alert.alert('개인정보처리방침')}>
+          개인정보처리방침
+        </TextLink>
+        을 읽고 이해했으며{'\n'}그에 동의함을 확인합니다.
+      </Text>
+    </LoginButtonWrapper>
   );
 };
 
 export default LoginContainer;
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const LoginButtonWrapper = styled.View`
+  margin: 16px 37px;
 `;
 
-const Button = styled.TouchableOpacity``;
+interface ILoginButtonProps extends TouchableWithoutFeedbackProps {
+  type: 'apple' | 'kakao';
+}
 
-const Text = styled.Text``;
+const LoginButton: React.FC<ILoginButtonProps> = ({ type, onPress }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={onPress}
+      style={{
+        height: 54,
+        marginBottom: 8,
+        borderRadius: 8,
+        backgroundColor: `${type === 'apple' ? '#000' : '#FFE812'}`,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        // justifyContent: 'space-between',
+        // display: 'inline',
+      }}>
+      <Image
+        source={
+          type === 'apple'
+            ? require(`../../assets/icons/button/logo_apple.png`)
+            : require(`../../assets/icons/button/logo_kakao.png`)
+        }
+        style={{
+          width: 24,
+          height: 24,
+          marginRight: 4,
+          resizeMode: 'contain',
+        }}
+      />
+      <Text
+        numberOfLines={1}
+        adjustsFontSizeToFit
+        style={{
+          color: `${type === 'apple' ? '#fff' : '#000'}`,
+          fontWeight: '600',
+          fontSize: 16,
+          letterSpacing: -0.6,
+        }}>
+        {type === 'apple' ? '애플' : '카카오'}로 시작하기
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const TextLink: React.FC<TouchableWithoutFeedbackProps> = ({
+  onPress,
+  children,
+}) => {
+  return (
+    <Text
+      onPress={onPress}
+      numberOfLines={1}
+      adjustsFontSizeToFit
+      style={{ textDecorationLine: 'underline' }}>
+      {children}
+    </Text>
+  );
+};
