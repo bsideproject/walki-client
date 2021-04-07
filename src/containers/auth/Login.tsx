8911ef7d4e03c18f304890cc3ed1ddef1d54711b
choon <chachooon@gmail.com>
2021-04-08 00:22:22 +0900

    feat: apple login

--- src/containers/auth/Login.tsx
+++ src/containers/auth/Login.tsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from '@emotion/native';
 import { Alert, Text } from 'react-native';
 import LoginButton from '../../components/LoginButton';
 import TextLink from '../../components/TextLink';
 import { INaviProps } from '../../navigators/AuthStackNavi';
+import { KakaoOAuthToken, login } from '@react-native-seoul/kakao-login';
+import { appleAuth } from '@invertase/react-native-apple-authentication';
 /**
  * 로그인 버튼
  * @param currIndex
  * @param setCurrIndex
  */
 const LoginContainer = ({ goNext }: INaviProps) => {
+  const onPressKakaoLogin = useCallback(async () => {
+    const token: KakaoOAuthToken = await login();
+    console.log('user is authenticated', token);
+    if (typeof goNext === 'function' && token) goNext();
+  }, []);
+
+  const onAppleButtonPress = useCallback(async () => {
+    try {
+      // Start the sign-in request
+      const appleAuthRequestResponse = await appleAuth.performRequest({
+        requestedOperation: appleAuth.Operation.LOGIN,
+        requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+      });
+      // get current authentication state for user
+      const credentialState = await appleAuth.getCredentialStateForUser(
+        appleAuthRequestResponse.user,
+      );
+      // use credentialState response to ensure the user is authenticated
+      if (credentialState === appleAuth.State.AUTHORIZED) {
+        console.log('user is authenticated', appleAuthRequestResponse);
+        if (typeof goNext === 'function') goNext();
+      }
+    } catch (error) {
+      if (error.code === appleAuth.Error.CANCELED) {
+        // login canceled
+      } else {
+        // login error
+      }
+    }
+    if (typeof goNext === 'function' && identityToken) goNext();
+  }, []);
+
   return (
     <BottomContainer>
       <LoginButtonWrapper>
-        <LoginButton
-          type="kakao"
-          onPress={() => {
-            Alert.alert('카카오로 시작하기');
-            if (typeof goNext === 'function') goNext();
-          }}
-        />
-        <LoginButton
-          type="apple"
-          onPress={() => {
-            Alert.alert('애플로 시작하기');
-            if (typeof goNext === 'function') goNext();
-          }}
-        />
+        <LoginButton type="kakao" onPress={onPressKakaoLogin} />
+        <LoginButton type="apple" onPress={onAppleButtonPress} />
 
         <Text
           numberOfLines={2}
